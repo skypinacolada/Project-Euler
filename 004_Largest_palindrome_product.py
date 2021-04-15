3c58c8457cdcdecbcda93911857b63b803dab2df
Lise <skypinacolada@gmail.com>
2021-04-15 16:04:11 +0900

    Change condition order

--- 004_Largest_palindrome_product.py
+++ 004_Largest_palindrome_product.py
@@ -5,6 +5,9 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 
+import datetime
+start = datetime.datetime.now()
+
 
 def is_palindrome(num):
     length = len(str(num))-1
@@ -17,10 +20,12 @@ def maxi_tri_palindrome():
     maxi = 0
     for n1 in range(999, 100, -1):
         for n2 in range(999, 100, -1):
-            if is_palindrome(n1*n2) and n1*n2 >= maxi : maxi = n1*n2
+            if n1*n2 >= maxi and is_palindrome(n1*n2) : maxi = n1*n2
     return maxi
 
 
 print(maxi_tri_palindrome())
+print(datetime.datetime.now() - start)
 
+#0.01
 
